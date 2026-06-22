fd420354a2b9487617039341560a26a92015d3b4
OmkarTerbhai <omkarterbhai@gmail.com>
2026-06-22 20:38:23 +0530

    Added codes for string

---
+++ lesson03/Program1.py
@@ -0,0 +1,24 @@
+#Strings in Python
+
+
+first_name = "Omkar";
+last_name = "Terbhai";
+
+print(first_name + " " + last_name);
+
+str1 = str("Hello");
+str2 = str(234);
+
+print(str1 + " " + str2);
+
+print(type(str1));
+print(type(str2));
+
+multi_line_str = """hello there buddy
+                    how are things going?
+                all good?""";
+
+print(multi_line_str);
+print(multi_line_str.upper());
+print(multi_line_str.replace("good", "ok"));
+print(multi_line_str.title());
