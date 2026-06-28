3fa1915c6d9bbfb8d16634a76f668e2642ae425d
OmkarTerbhai <omkarterbhai@gmail.com>
2026-06-28 18:02:15 +0530

    Initial Commit

---
+++ lesson07/Program1.py
@@ -0,0 +1,18 @@
+# Loops in Python
+
+my_list = list([20, 30, 40, 50]);
+
+for i in my_list :
+    print(i, end=" ");
+
+print("-------------------------------");
+
+# for a in range(10, 101, 5) :
+#     print(a);
+#
+num = 10;
+
+while num <= 17 :
+    print(num);
+
+    num += 1;
